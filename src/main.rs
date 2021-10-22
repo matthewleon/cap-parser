572552c30225e3ca8b334bcda695e9e974fd13fe
Matthew Leon <ml@matthewleon.com>
2021-10-22 14:44:53 -0400

    Kill tesseract usage.
    Only use a rendering pipeline.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,12 @@ extern crate derivative;
 use std::fs;
 use fs::File;
 use std::io::{Read, Write};
-use std::path::Path;
 
-use image::ImageError;
-use leptess::capi;
-use leptess::leptonica::pix_read;
-use leptess::tesseract::TessApi;
+use image::ImageResult;
 use nom::error::VerboseError;
 
 use crate::parser::parse::packet;
 use crate::parser::renderer::{Handler, Screen};
-use nom::lib::std::collections::BTreeMap;
-use std::sync::{Arc, Mutex};
 use threadpool::ThreadPool;
 
 #[cfg(not(target_env = "msvc"))]
@@ -56,7 +50,6 @@ fn do_parse(i: &[u8]) -> String {
 
     let mut rest = i;
 
-    let out = Arc::new(Mutex::new(BTreeMap::new()));
     let pool = ThreadPool::new(num_cpus::get());
     while !rest.is_empty() {
         match packet::<VerboseError<&[u8]>>(&rest) {
@@ -65,16 +58,8 @@ fn do_parse(i: &[u8]) -> String {
                 match handler.handle(packet) {
                     Ok(image) => match image {
                         Some(img) => {
-                            let out = out.clone();
                             pool.execute(move || match display_to_text(frame, &img) {
-                                Ok(res) => {
-                                    match res {
-                                        Some(data) => {
-                                            out.lock().unwrap().insert(frame, data);
-                                        },
-                                        None => {}
-                                    };
-                                }
+                                Ok(()) => {}
                                 Err(error) => eprintln!("error {:#?}\n", error),
                             });
                             frame = frame + 1;
@@ -94,91 +79,19 @@ fn do_parse(i: &[u8]) -> String {
         }
     }
     pool.join();
-
-    let lines: Vec<String> = Arc::try_unwrap(out)
-        .unwrap()
-        .into_inner()
-        .unwrap()
-        .into_iter()
-        .map(|(_, v)| v)
-        .collect();
-    lines.join("\n")
+    return String::new();
 }
 
-fn post_process_text(text: String) -> Option<String> {
-    let mut out = text;
-    out = out.replace("|", "I");
-    out = out.split("\n")
-        .into_iter()
-        .map(|line| line.replace("\n", " "))
-        .map(|line| line.trim_start().trim_end().to_string())
-        .filter(|line| !line.is_empty())
-        .collect::<Vec<String>>()
-        .join("\n");
-
-    if out.is_empty() {
-        None
-    } else {
-        Some(out)
-    }
-}
-
-fn display_to_text(frame: u32, d: &Screen) -> Result<Option<String>, ImageError> {
-    const LANG: &str = "eng";
-    // TODO: thread local storage would probably be beneficial here
-    let mut tess = TessApi::new(None, LANG).unwrap();
-
-    unsafe {
-        capi::TessBaseAPISetPageSegMode(
-            tess.raw,
-            leptess::capi::TessPageSegMode_PSM_SPARSE_TEXT_OSD,
-        );
-    };
-
+fn display_to_text(frame: u32, d: &Screen) -> ImageResult<()> {
     let fname = format!("tmp/sub-{}.tiff", frame);
-    d.image.save(&fname)?;
+    d.image.save(&fname)
 
-    let pix = pix_read(Path::new(&fname)).unwrap();
-    tess.set_image(&pix);
-    unsafe {
-        capi::TessBaseAPISetSourceResolution(tess.raw, 120);
-    }
-    let text = post_process_text(tess.get_utf8_text().unwrap());
-
-    match fs::remove_file(&fname) {
-        Ok(_) => {},
-        Err(err) => {
-            eprintln!("error deleting frame temp file {} -> {}", fname, err)
-        },
-    }
-
-    Ok(text.map(|data| format!(
-        "{}\n{} --> {}\n{}\n\n",
-        frame + 1,
-        format_timestamp_microsec(d.begin_mis),
-        format_timestamp_microsec(d.begin_mis + d.dur_mis),
-        data
-    )))
-}
-
-fn format_timestamp_microsec(ms: u64) -> String {
-    const MS_PER_MICRO: u64 = 1_000;
-    const SEC_PER_MICRO: u64 = MS_PER_MICRO * 1_000;
-    const MIN_PER_MICRO: u64 = SEC_PER_MICRO * 60;
-    const HR_PER_MICRO: u64 = MIN_PER_MICRO * 60;
-
-    let mut ms_remain = ms;
-
-    let hours = ms_remain / HR_PER_MICRO;
-    ms_remain = ms_remain % HR_PER_MICRO;
-
-    let minutes = ms_remain / MIN_PER_MICRO;
-    ms_remain = ms_remain % MIN_PER_MICRO;
-
-    let seconds = ms_remain / SEC_PER_MICRO;
-    ms_remain = ms_remain % SEC_PER_MICRO;
-
-    let milliseconds = ms_remain / MS_PER_MICRO;
-
-    return format!("{}:{}:{},{}", hours, minutes, seconds, milliseconds);
+    // TODO: finally output JSON with this info
+    // Ok(text.map(|data| format!(
+    //     "{}\n{} --> {}\n{}\n\n",
+    //     frame + 1,
+    //     format_timestamp_microsec(d.begin_mis),
+    //     format_timestamp_microsec(d.begin_mis + d.dur_mis),
+    //     data
+    // )))
 }
